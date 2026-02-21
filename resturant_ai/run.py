a27e3d0b7eda5bfe456e6380e9f49511279a6f77
Ayushkrity Anuj <ayushkrityanuj@Ayushkritys-MacBook-Air.local>
2026-02-21 14:48:20 +0530

    restructure resturant repo

--- resturant_ai/run.py
+++ resturant_ai/run.py
@@ -1,11 +1,16 @@
-import streamlit.web.cli as stcli
+import subprocess
 import sys
 import os
 
 def main():
-    sys.argv = [
-        "streamlit",
-        "run",
-        os.path.join(os.path.dirname(__file__), "..", "app.py"),
-    ]
-    sys.exit(stcli.main())
+    app_path = os.path.join(os.path.dirname(__file__), "app.py")
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "streamlit",
+            "run",
+            app_path,
+        ]
+    )
